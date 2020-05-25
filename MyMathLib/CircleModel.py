9390235c33d85c0a4cf28b4599b2948311c78458
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-05-25 03:00:20 +0100

    Understood @classmethod

--- MyMathLib/CircleModel.py
+++ MyMathLib/CircleModel.py
@@ -15,3 +15,46 @@ class CircleModel(object):
         display=("X=%f Y=%f R=%f" % (self.X,self.Y,self.R))
         return display
 
+    @classmethod
+    def GenerateModelFrom3Points(cls,p1:Point, p2:Point, p3:Point):
+        x1=p1.X
+        x2=p2.X
+        x3=p3.X
+        y1=p1.Y
+        y2=p2.Y
+        y3=p3.Y
+        c = (x1-x2)**2 + (y1-y2)**2
+        a = (x2-x3)**2 + (y2-y3)**2
+        b = (x3-x1)**2 + (y3-y1)**2   
+        s = 2*(a*b + b*c + c*a) - (a*a + b*b + c*c)
+
+        px = (a*(b+c-a)*x1 + b*(c+a-b)*x2 + c*(a+b-c)*x3) / s
+        py = (a*(b+c-a)*y1 + b*(c+a-b)*y2 + c*(a+b-c)*y3) / s 
+        ar = a**0.5
+        br = b**0.5
+        cr = c**0.5 
+        r = ar*br*cr / ((ar+br+cr)*(-ar+br+cr)*(ar-br+cr)*(ar+br-cr))**0.5
+
+        circ= cls(px,py,r)
+    
+        return circ
+
+        #
+    #Computes points which lie on the given circle model 
+    #Returns a list of points
+    #
+    @classmethod
+    def generate_points_from_circle(model):
+        angleStart=0
+        angleEnd=2*3.1415
+        num=100
+        angles=np.linspace(angleStart,angleEnd,num)
+        radius=model.R
+        lst_points:List[pt.Point]=list()
+        for idx in range(0,len(angles)):
+            theta=angles[idx]
+            x=radius * math.cos(theta)  +model.X
+            y=radius * math.sin(theta)  +model.Y
+            pt_new=pmodel.Point(x,y)
+            lst_points.append(pt_new)
+        return lst_points
--- Tests/test_CircleModel.py
+++ Tests/test_CircleModel.py
@@ -18,6 +18,20 @@ class Test_CircleModel(unittest.TestCase):
         self.assertTrue(display.find("Y=200.2") >= 0)
         self.assertTrue(display.find("R=300.3") >= 0)
 
+    #Points  (-1,0) (0,1)  (1,0)
+    def test_GenerateModelFrom3pmodels_PassingThrough_1_0_And_0_1_minus1_0(self):
+        p_1_0=Point(0,1)
+        p_0_0=Point(1,0)
+        p_minus1_0=Point(-1,0)
+
+
+        c1=CircleModel.GenerateModelFrom3Points(p_0_0,p_1_0,p_minus1_0)
+        #Assert on radius
+        self.assertAlmostEquals(c1.R, 1.0,1)
+        #Assert on center X,Y
+        self.assertAlmostEquals(c1.X, 0.0,1)
+        self.assertAlmostEquals(c1.Y, 0.0,1)
+
 
 if __name__ == '__main__':
     unittest.main()
